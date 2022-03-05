b18acec229538a6df4581bf8da5abd487c0cf8d1
Mustafa Açıkyol <95076715+mustafaacikyol@users.noreply.github.com>
2022-03-05 21:54:29 +0300

    Add files via upload

---
+++ homeworks/homework_1/task_2/build_CMake/CMakeFiles/ipb_arithmetic.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/users/mustafa-user/desktop/c++/homeworks/homework_1/task_2/src/subtract.cpp" "/mnt/c/users/mustafa-user/desktop/c++/homeworks/homework_1/task_2/build_CMake/CMakeFiles/ipb_arithmetic.dir/src/subtract.cpp.o"
+  "/mnt/c/users/mustafa-user/desktop/c++/homeworks/homework_1/task_2/src/sum.cpp" "/mnt/c/users/mustafa-user/desktop/c++/homeworks/homework_1/task_2/build_CMake/CMakeFiles/ipb_arithmetic.dir/src/sum.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
