bf748f8703ffb0272ea08964ae08fedd697026c7
Mustafa Açıkyol <95076715+mustafaacikyol@users.noreply.github.com>
2022-04-22 21:22:09 +0300

    Add files via upload

---
+++ homeworks/homework_3/build3/CMakeFiles/main3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main3.dir/mnt/d/c++/homeworks/homework_3/src/main3.cpp.o"
+  "main3"
+  "main3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
