86b2ab199cb440d373924dccc4554975875cd0ab
Mustafa Açıkyol <95076715+mustafaacikyol@users.noreply.github.com>
2022-04-22 21:20:59 +0300

    Add files via upload

---
+++ homeworks/homework_3/build2/CMakeFiles/image_html.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/image_html.dir/mnt/d/c++/homeworks/homework_3/src/html_writer.cpp.o"
+  "CMakeFiles/image_html.dir/mnt/d/c++/homeworks/homework_3/src/image_browser.cpp.o"
+  "libimage_html.a"
+  "libimage_html.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/image_html.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
